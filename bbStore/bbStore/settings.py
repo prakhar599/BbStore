b94c3da4220ad557270d08cc8f366ef9a400c325
Prakhar <forestlaw319@gmail.com>
2024-01-12 20:19:21 +0530

    Fixed security concerns

--- bbStore/bbStore/settings.py
+++ bbStore/bbStore/settings.py
@@ -1,23 +1,12 @@
 import os
 from pathlib import Path
+from decouple import config
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vf^gw^9uiob016vic7er8))p4yn6@xqrrd&troe0lbg@z0^q*%'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['prakhar13.pythonanywhere.com']
-
-
-# Application definition
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-vf^gw^9uiob016vic7er8))p4yn6@xqrrd&troe0lbg@z0^q*%')
+DEBUG = config('DEBUG', default=True, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=lambda v: [s.strip() for s in v.split(',')])
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,20 +50,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bbStore.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DATABASE_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': config('DATABASE_NAME', default=os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -91,55 +72,30 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = 'home/prakhar13/static'
-STATIC_DIR = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"static"),
-    os.path.join(BASE_DIR,"blog/static"),
-    os.path.join(BASE_DIR, "static/ckeditor"),  # Add this line
-
-]
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+STATIC_URL = config('STATIC_URL', default='/static/')
+STATIC_ROOT = config('STATIC_ROOT', default=os.path.join(BASE_DIR, 'static'))
+STATIC_DIR = config('STATIC_DIR', default=os.path.join(BASE_DIR, 'static'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_URL = config('MEDIA_URL', default='/media/')
+MEDIA_ROOT = config('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media/'))
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
-
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
 
-CKEDITOR_UPLOAD_PATH = '/media/'
+CKEDITOR_UPLOAD_PATH = config('CKEDITOR_UPLOAD_PATH', default='/media/')
 
-PAYPAL_CLIENT_ID = 'ATwFdKf9EdYIEavSFvN7lx0qy3j4XgcWwozVZovLhrjrcvWovCefR5xr3tnhhvQKIlDeqdoKTpc-t6oG'
-PAYPAL_SECRET = 'ECZe1ALQwuRRemYgPhFlwnu_I97K3bGZsr1IiElusXIltq9f3gn5GnUIev88L0fEzCSji4jzqNMZqLyl'
+# PayPal Settings
+PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID', default='')
+PAYPAL_SECRET = config('PAYPAL_SECRET', default='')
